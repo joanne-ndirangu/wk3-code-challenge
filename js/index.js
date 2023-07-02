498bab1aa07a0f5130dbf0042cbbc0b69dbb7f2f
Joanne Ndirangu <joannendirangu15@gmail.com>
2023-07-03 01:56:17 +0300

    Updates code-challenge

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
-// document.addEventListener('DOMContentLoaded', () => {
-
-fetch('http://localhost:3000/films/1')
+//make a request for the first film
+fetch(`http://localhost:3000/films/1`)
 .then(res => res.json())
 .then(firstFilm => {
   displayFirstFilm(firstFilm);
 
+//display first film details as default when page loads
   function displayFirstFilm(film) {
     let firstFilmElement = document.getElementById('first-film');
     const availableTickets = film.capacity - film.tickets_sold
@@ -15,15 +15,16 @@ fetch('http://localhost:3000/films/1')
       <p>Runtime: ${film.runtime} minutes</p>
       <p>Showtime: ${film.showtime}</p>
       <p class="tickets">Available tickets: ${availableTickets}</p>
-      <button onclick="buyTicket(${film.id})">Buy ticket</button>
+      <button class="buy-button" onclick="buyTicket(${film.id})">Buy ticket</button>
+      <button class="delete">Delete</button>
     `
   }
 })
-// })
 
+//Create list of all film titles
 const filmList = document.getElementById('films')
 
-fetch('http://localhost:3000/films')
+fetch(`http://localhost:3000/films`)
   .then(res => res.json())
   .then(films => {
     films.forEach(film => {
@@ -36,25 +37,39 @@ fetch('http://localhost:3000/films')
     })
   })
 
+//Display details of the selected film in place of the default first film
 function filmDetails(film) {
-  const availableTickets = film.capacity - film.tickets_sold;
-  filmList.innerHTML = `
-    <img class="pic" src="${film.poster}" alt="${film.title}">
-    <h2>${film.title}</h2>
-    <p>${film.description}</p>
-    <p>Runtime: ${film.runtime} minutes</p>
-    <p>Showtime: ${film.showtime}</p>
-    <p class="tickets">Available tickets: ${availableTickets}</p>
-    <button onclick="buyTicket(${film.id})">Buy ticket</button>
-  `
-}
+    fetch(`http://localhost:3000/films/${film.id}`)
+      .then(res => res.json())
+      .then(selectedFilm => {
+        const availableTickets = selectedFilm.capacity - selectedFilm.tickets_sold;
+        let firstFilmElement = document.getElementById('first-film');
+        firstFilmElement.innerHTML = `
+          <img class="pic" src="${selectedFilm.poster}" alt="${selectedFilm.title}">
+          <h2>${selectedFilm.title}</h2>
+          <p>${selectedFilm.description}</p>
+          <p>Runtime: ${selectedFilm.runtime} minutes</p>
+          <p>Showtime: ${selectedFilm.showtime}</p>
+          <p class="tickets">Available tickets: ${availableTickets}</p>
+          <button class="buy-button" onclick="buyTicket(${selectedFilm.id})">Buy ticket</button>
+          <button class="delete">Delete</button>
+        `;
+      })
+    }
 
-function buyTicket() {
-    let filmDetails = document.getElementById('films')
-    let ticketsElem = filmDetails.querySelector('.tickets')
-    let availableTickets = parseInt(ticketsElem.textContent.split(': ')[1])
-    if (availableTickets > 0)
-    ticketsElem.textContent = `Available tickets: ${--availableTickets}`
-    } if (availableTickets === 0) {
-          alert("Sold out")
+//Function to reduce available details for every ticket bought
+    function buyTicket(filmId) {
+        let ticketsElem = document.querySelector('.tickets');
+        let availableTickets = parseInt(ticketsElem.textContent.split(': ')[1])
+
+//Once available tickets get to zero, buy ticket button changes to sold out
+        if (availableTickets > 0) {
+          ticketsElem.textContent = `Available tickets: ${--availableTickets}`
+          if (availableTickets === 0) {
+            const buyButton = document.querySelector('.buy-button')
+            buyButton.textContent = 'Sold Out'
+            buyButton.disabled = true
+            filmItem.classList.add('sold-out')
+          }
         }
+    }
